b1c1b8a9cedac2f08c6dbe1976225af2db23cf7f
duongdx <dinh.xuan.duong@sun-asterisk.com>
2022-09-01 02:43:50 +0700

    add node

--- aws/ec2-user-data.sh
+++ aws/ec2-user-data.sh
@@ -5,4 +5,4 @@ yum update -y
 yum install -y httpd
 systemctl start httpd
 systemctl enable httpd
-echo "<h1> Hello world</h1>" > /var/www/html/index.html
+echo "<h1> Hello world $(hostname -f)</h1>" > /var/www/html/index.html
